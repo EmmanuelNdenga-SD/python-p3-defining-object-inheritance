b32c21439f1f2fd7fcde92cb57e5c0cde321315e
emmanue-ndenga <emmanuel.ndenga@student.moringaschool.com>
2025-05-18 12:02:51 +0300

    adds car.py and vehicle.py

--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,10 @@
 class Vehicle:
+    def  __init__(self,wheel_size, wheeel_number):
+        self.wheel_size = wheel_size
+        self.wheel_number = wheeel_number
+
+    def go(self):
+        return "vrrrrrrrooom!"
     pass
+    def fill_up_tank(self):
+        return "filling up!"
